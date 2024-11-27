e2cda800b92bd608fbb977c7ac8c18d9a8cb10a3
Aarav Garg <gargaarav79@gmail.com>
2024-11-26 21:16:50 -0500

    update

--- code/code.ino
+++ code/code.ino
@@ -1,1 +1,30 @@
+#include <Servo.h>
 
+Servo left_hip_pitch;
+Servo left_hip_yaw;
+Servo left_hip_roll;
+Servo left_knee;
+
+Servo right_hip_pitch;
+Servo right_hip_yaw;
+Servo right_hip_roll;
+Servo right_knee;
+
+void setup(){
+
+left_hip_pitch.attach(5)
+left_hip_roll.attach(6);
+left_knee.attach(3);
+// left_hip_yaw.attach(NA);
+
+right_hip_pitch.attach(10);
+right_hip_roll.attach(9);
+right_knee.attach(11);
+// right_hip_yaw.attach(NA);
+
+
+}
+
+void loop(){
+
+}
